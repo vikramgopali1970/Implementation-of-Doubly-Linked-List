f9dda6f293b3c65686491b55ec40b15c9818983c
Vikram Gopali <vikramgopali.93@gmail.com>
2018-09-05 19:48:30 -0500

    individual tasks for implementing other actions on LL

--- Main.java
+++ Main.java
@@ -1,83 +1,153 @@
 package vxg180002;
 
+/**
+* created on Aug 27 2018
+*
+* edited on Aug 30 2018
+*
+* Assignment by:
+* Vikram Gopali (netId : vxg180002)
+*
+*/
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws NoSuchElementException {
-        DoublyLinkedList<Integer> dlst = new DoublyLinkedList<>();
-        for (int i=0;i<10;i++){
-            dlst.add(i);
+        int n = 10;
+        if(args.length > 0) {
+            n = Integer.parseInt(args[0]);
         }
 
-        Iterator<Integer> it = dlst.iterator();
-        while(it.hasNext()){
-            Scanner in = new Scanner(System.in);
-            whileloop:
-            while(in.hasNext()) {
-                int com = in.nextInt();
-                switch(com) {
-                    case 1:  // Move to next element and print it
-                        if (it.hasNext()) {
-                            System.out.println(it.next());
-                        } else {
-                            break whileloop;
-                        }
-                        break;
-                    case 2:  // Remove element
-                        it.remove();
-                        dlst.printList();
-                        break;
-                    case 3:  // Move to previous element and print it
-                        System.out.println(((DoublyLinkedList<Integer>.DLLIterator)it).prev());
-                        break;
-                    case 4:  // add element before returning the element in next() and print the list
-                        System.out.println("Please enter the value you want to enter");
-                        ((DoublyLinkedList<Integer>.DLLIterator)it).add(in.nextInt());
-                        dlst.printList();
-                        break;
-                    case 5:  // add element at the tail
-                        System.out.println("Please enter the value you want to enter");
-                        dlst.add(in.nextInt());
-                        dlst.printList();
-                        break;
-                    default:  // Exit loop
+        SinglyLinkedList<Integer> lst = new SinglyLinkedList<>();
+        for(int i=1; i<=n; i++) {
+            lst.add(Integer.valueOf(i));
+        }
+        lst.printList();
+
+        Iterator<Integer> it = lst.iterator();
+        Scanner in = new Scanner(System.in);
+        whileloop:
+        while(in.hasNext()) {
+            int com = in.nextInt();
+            int ind;
+            switch(com) {
+                case 1:  // Move to next element and print it
+                    if (it.hasNext()) {
+                        System.out.println(it.next());
+                    } else {
                         break whileloop;
-                }
+                    }
+                    break;
+                case 2:  // Remove element
+                    it.remove();
+                    lst.printList();
+                    break;
+                case 3:  // adds element in the beginning
+                    System.out.println("enter the value of the node");
+                    lst.addFirst(in.nextInt());
+                    lst.printList();
+                    break;
+                case 4:  // Remove first element
+                    lst.removeFirst();
+                    lst.printList();
+                    break;
+                case 5:  // Remove a element at a particular index
+                    System.out.println("enter the index of the node to be deleted");
+                    lst.remove(in.nextInt());
+                    lst.printList();
+                    break;
+                case 6:  // get element at index
+                    System.out.println("enter the index to be retrieved");
+                    System.out.println("element is "+lst.get(in.nextInt()));
+                    break;
+                case 7:  // set element at index
+                    System.out.println("enter the index to be set");
+                    ind = in.nextInt();
+                    System.out.println("enter the value to be set");
+                    lst.set(ind,in.nextInt());
+                    lst.printList();
+                    break;
+                case 8:  // add an element at particular index
+                    System.out.println("enter the index to be add new node");
+                    ind = in.nextInt();
+                    System.out.println("enter the value to be added");
+                    lst.add(ind,in.nextInt());
+                    lst.printList();
+                    break;
+                case 9:  // remove node at a particular index
+                    System.out.println("enter the index to delete");
+                    lst.remove(in.nextInt());
+                    lst.printList();
+                    break;
+                default:  // Exit loop
+                    break whileloop;
             }
         }
+        lst.printList();
+        lst.unzip();
+        lst.printList();
     }
 }
 
 
 
 /*
-* Doubly Linked List
-* Sample Input:
-* 0,1,2,3,4,5,6,7,8,9
-*
-* Sample Output:
-* 1
-* 0
-* 1
-* 1
-* 1
-* 2
-* 2
-* 9: 0 1 3 4 5 6 7 8 9
-* 3
-* 1
-* 3
-* 0
-* 4
-* Please enter the value you want to enter
-* 456
-* 10: 0 456 1 3 4 5 6 7 8 9
-* 1
-* 456
-* 1
-* 1
-* 1
-* 3
-* */
+
+Sample Input :
+1,2,3,4,5,6,7,8,9,10
+
+Sample Output :
+10: 1 2 3 4 5 6 7 8 9 10
+1
+1
+1
+2
+1
+3
+1
+4
+2
+9: 1 2 3 5 6 7 8 9 10
+1
+5
+1
+6
+3
+enter the value of the node
+4445
+10: 4445 1 2 3 5 6 7 8 9 10
+3
+enter the value of the node
+333
+11: 333 4445 1 2 3 5 6 7 8 9 10
+4
+10: 4445 1 2 3 5 6 7 8 9 10
+5
+enter the node to be deleted
+9
+9: 4445 1 2 3 5 6 7 8 9
+6
+enter the index to be retrieved
+4
+element is 5
+7
+enter the index to be set
+4
+enter the value to be set
+666
+9: 4445 1 2 3 666 6 7 8 9
+8
+enter the index to be add new node
+2
+enter the value to be added
+444
+10: 4445 1 444 2 3 666 6 7 8 9
+9
+enter the index to delete
+7
+8: 4445 1 444 2 3 666 6 8 9
+
+ */
--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,4 +1,14 @@
 
+/**
+ * created on Aug 27 2018
+ *
+ * edited on Aug 30 2018
+ *
+ * Assignment by:
+ * Vikram Gopali (netId : vxg180002)
+ *
+ */
+
 
 package vxg180002;
 import java.util.Iterator;
@@ -6,7 +16,9 @@ import java.util.NoSuchElementException;
 
 public class SinglyLinkedList<T> implements Iterable<T> {
 
-    /** Class Entry holds a single node of the list */
+    /**
+     * Class Entry holds a single node of the list
+     */
     static class Entry<E> {
         E element;
         Entry<E> next;
@@ -27,7 +39,9 @@ public class SinglyLinkedList<T> implements Iterable<T> {
         size = 0;
     }
 
-    public Iterator<T> iterator() { return new SLLIterator(); }
+    public Iterator<T> iterator() {
+        return new SLLIterator();
+    }
 
     protected class SLLIterator implements Iterator<T> {
         Entry<T> cursor, prev;
@@ -44,27 +58,33 @@ public class SinglyLinkedList<T> implements Iterable<T> {
         }
 
         public T next() {
-            prev = cursor;
-            cursor = cursor.next;
-            ready = true;
-            return cursor.element;
+            if (hasNext()) {
+                prev = cursor;
+                cursor = cursor.next;
+                ready = true;
+                return cursor.element;
+            } else {
+                return null;
+            }
         }
 
         // Removes the current element (retrieved by the most recent next())
         // Remove can be called only if next has been called and the element has not been removed
         public void remove() {
-            if(!ready) {
+            if (!ready) {
                 throw new NoSuchElementException();
             }
             prev.next = cursor.next;
             // Handle case when tail of a list is deleted
-            if(cursor == tail) {
+            if (cursor == tail) {
                 tail = prev;
             }
             cursor = prev;
             ready = false;  // Calling remove again without calling next will result in exception thrown
             size--;
         }
+
+
     }  // end of class SLLIterator
 
     // Add new elements to the end of the list
@@ -78,20 +98,117 @@ public class SinglyLinkedList<T> implements Iterable<T> {
         size++;
     }
 
+    public void addFirst(T x) {
+        head.next = new Entry<>(x, head.next);
+        size++;
+    }
+
+    public T removeFirst() {
+        T removedElem;
+        if (null != head.next) {
+            removedElem = head.next.element;
+            head.next = head.next.next;
+            size--;
+            return removedElem;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    public void remove(T x) {
+        SLLIterator it = new SLLIterator();
+        while (it.hasNext() && x != it.next()) {
+            it.next();
+        }
+        if (null != it.next()) {
+            it.remove();
+        } else {
+            throw new NoSuchElementException();
+        }
+        it.ready = false;
+    }
+
     public void printList() {
         System.out.print(this.size + ": ");
-        for(T item: this) {
+        for (T item : this) {
             System.out.print(item + " ");
         }
 
         System.out.println();
     }
 
+    public boolean IndexOutOfRange(int index) {
+        return index >= size;
+    }
+
+    public T get(int index) {
+        SLLIterator it = new SLLIterator();
+        if (this.IndexOutOfRange(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+        while (index > 0) {
+            it.next();
+            index--;
+        }
+        return it.next();
+    }
+
+    public void set(int index, T x) {
+        Entry<T> cur = head;
+        if (this.IndexOutOfRange(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+        while (index >= 0) {
+            cur = cur.next;
+            index--;
+        }
+        cur.element = x;
+    }
+
+    public void add(int index, T x) {
+        Entry<T> cur = head;
+        if (this.IndexOutOfRange(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+        while (index > 0) {
+            cur = cur.next;
+            index--;
+        }
+        cur.next = new Entry<>(x, cur.next);
+        size++;
+    }
+
+    public T remove(int index) {
+        SLLIterator it = new SLLIterator();
+        T elemRemoved;
+        if (this.IndexOutOfRange(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+        while (index >= 0) {
+            it.next();
+            index--;
+        }
+        if (it.ready) {
+            if (it.cursor == tail) {
+                it.prev.next = null;
+                elemRemoved = it.cursor.element;
+                it.cursor = it.prev;
+            } else {
+                elemRemoved = it.cursor.next.element;
+                it.remove();
+            }
+            size--;
+            return elemRemoved;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
     // Rearrange the elements of the list by linking the elements at even index
     // followed by the elements at odd index. Implemented by rearranging pointers
     // of existing elements without allocating any new elements.
     public void unzip() {
-        if(size < 3) {  // Too few elements.  No change.
+        if (size < 3) {  // Too few elements.  No change.
             return;
         }
 
@@ -106,8 +223,8 @@ public class SinglyLinkedList<T> implements Iterable<T> {
         // c is current element to be processed.
         // state indicates the state of the finite state machine
         // state = i indicates that the current element is added after taili (i=0,1).
-        while(c != null) {
-            if(state == 0) {
+        while (c != null) {
+            if (state == 0) {
                 tail0.next = c;
                 tail0 = c;
                 c = c.next;
